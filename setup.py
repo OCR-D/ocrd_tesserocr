67247e2510bf14bcdedb24bb2021a9d60ff9f616
Konstantin Baierer <unixprog@gmail.com>
2018-04-19 00:19:39 +0200

    depend on pyocrd >= 0.0.4

--- setup.py
+++ setup.py
@@ -13,9 +13,6 @@ from setuptools import setup, find_packages
 with codecs.open('README.rst', encoding='utf-8') as f:
     README = f.read()
 
-with codecs.open('LICENSE', encoding='utf-8') as f:
-    LICENSE = f.read().encode('utf-8')
-
 setup(
     name='ocrd_tesserocr',
     version='0.0.1',
@@ -27,15 +24,15 @@ setup(
     license='Apache License 2.0',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
-        'ocrd >= 0.0.2',
+        'ocrd >= 0.0.4',
         'click',
         'tesserocr',
     ],
     entry_points={
         'console_scripts': [
-            'ocrd_tesserocr_segment_region=ocrd_tesserocr.cli:ocrd_tesserocr_segment_region',
-            'ocrd_tesserocr_segment_line=ocrd_tesserocr.cli:ocrd_tesserocr_segment_line',
-            'ocrd_tesserocr_recognize=ocrd_tesserocr.cli:ocrd_tesserocr_recognize',
+            'ocrd-tesserocr-segment-region=ocrd_tesserocr.cli:ocrd_tesserocr_segment_region',
+            'ocrd-tesserocr-segment-line=ocrd_tesserocr.cli:ocrd_tesserocr_segment_line',
+            'ocrd-tesserocr-recognize=ocrd_tesserocr.cli:ocrd_tesserocr_recognize',
         ]
     },
 )
