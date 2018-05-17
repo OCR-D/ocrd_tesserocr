29ce03ffb6ff4d41feda46437f4f658dad8f5a8a
Konstantin Baierer <unixprog@gmail.com>
2018-05-17 18:16:42 +0200

    require ocrd >= 0.2.3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     license='Apache License 2.0',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
-        'ocrd >= 0.2.0',
+        'ocrd >= 0.2.3',
         'click',
         'tesserocr',
     ],
