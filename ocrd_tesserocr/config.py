86e7dde36e575da9cf8cb4b8048bb7f36490eb44
Konstantin Baierer <unixprog@gmail.com>
2018-05-17 17:07:48 +0200

    json_loads expects string, not bytes

--- ocrd_tesserocr/config.py
+++ ocrd_tesserocr/config.py
@@ -6,4 +6,4 @@ import tesserocr
 
 TESSDATA_PREFIX = os.environ['TESSDATA_PREFIX'] if 'TESSDATA_PREFIX' in os.environ else tesserocr.get_languages()[0]
 
-OCRD_TOOL = json.loads(resource_string(__name__, 'ocrd-tool.json'))
+OCRD_TOOL = json.loads(resource_string(__name__, 'ocrd-tool.json').decode('utf8'))
