56fe10a4eb6c3284af4043a8cb8e4513243350a3
Konstantin Baierer <unixprog@gmail.com>
2018-06-22 03:04:03 +0200

    :up: require ocrd >= 0.4.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     license='Apache License 2.0',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
-        'ocrd >= 0.3.1',
+        'ocrd >= 0.4.0',
         'click',
         'ocrd-fork-tesserocr >= 3.0.0rc1',
     ],
