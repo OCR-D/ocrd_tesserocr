3e4622d17d82b3a825bbf3e6b5e65c0ecc1e1425
Konstantin Baierer <unixprog@gmail.com>
2018-08-06 16:15:36 +0200

    update cruft to fix CI

--- test/test_recognize.py
+++ test/test_recognize.py
@@ -19,7 +19,7 @@ class TestTesserocrRecognize(TestCase):
 
     skip("Takes too long")
     def runTest(self):
-        resolver = Resolver(cache_enabled=True)
+        resolver = Resolver()
         #  workspace = resolver.workspace_from_url(assets.url_of('SBB0000F29300010000/mets_one_file.xml'), directory=WORKSPACE_DIR)
         workspace = resolver.workspace_from_url(assets.url_of('kant_aufklaerung_1784-page-block-line-word/mets.xml'), directory=WORKSPACE_DIR)
         TesserocrSegmentRegion(
