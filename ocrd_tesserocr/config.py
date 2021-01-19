b2de1a7b87a69f36c7ab690589a82934e7ff0778
Konstantin Baierer <unixprog@gmail.com>
2021-01-19 19:04:34 +0100

    respect TESSDATA_PREFIX environment var

--- ocrd_tesserocr/config.py
+++ ocrd_tesserocr/config.py
@@ -7,8 +7,10 @@ import tesserocr
 
 from ocrd.resource_manager import OcrdResourceManager
 
-location = OcrdResourceManager().default_resource_dir
-MODEL_LOCATION = join(location, 'ocrd-tesserocr-recognize')
-MODEL_LOCATION_CROP = join(location, 'ocrd-tesserocr-crop')
+if 'TESSDATA_PREFIX' in os.environ:
+    MODEL_LOCATION = os.environ['TESSDATA_PREFIX']
+else:
+    location = OcrdResourceManager().default_resource_dir
+    MODEL_LOCATION = join(location, 'ocrd-tesserocr-recognize')
 
 OCRD_TOOL = json.loads(resource_string(__name__, 'ocrd-tool.json').decode('utf8'))
