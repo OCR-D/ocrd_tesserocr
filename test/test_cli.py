0567c5b6703ce83283bf54c242b4df6a757fcd7a
Konstantin Baierer <unixprog@gmail.com>
2022-08-13 14:29:46 +0200

    test_cli: do not clutter os.environ

--- test/test_cli.py
+++ test/test_cli.py
@@ -12,22 +12,19 @@ runner = CliRunner()
 def test_show_resource(tmpdir):
     samplefile = Path(tmpdir, 'bar.traineddata')
     samplefile.write_text('bar')
-    environ['TESSDATA_PREFIX'] = str(tmpdir)
-    import sys
-    r = runner.invoke(ocrd_tesserocr_recognize, ['-C', 'bar'], env=environ)
-    print(r)
+    r = runner.invoke(ocrd_tesserocr_recognize, ['-C', 'bar'], env={**environ, 'TESSDATA_PREFIX': str(tmpdir)})
     assert not r.exit_code
     # XXX doesn't work <del>because shutil.copyfileobj to stdout won't be captured
     # by self.invoke_cli</del> Not sure why it does not work :(
     # assert r.output == 'bar'
 
 def test_list_all_resources(tmpdir):
-        samplefile = Path(tmpdir, 'foo.traineddata')
-        samplefile.write_text('foo')
-        environ['TESSDATA_PREFIX'] = str(tmpdir)
-        r = runner.invoke(ocrd_tesserocr_recognize, ['-L'])
-        assert not r.exit_code
-        # assert r.output == str(samplefile) + '\n'
+    samplefile = Path(tmpdir, 'foo.traineddata')
+    samplefile.write_text('foo')
+    r = runner.invoke(ocrd_tesserocr_recognize, ['-L'], env={**environ, 'TESSDATA_PREFIX': str(tmpdir)})
+    assert not r.exit_code
+    # XXX same problem
+    # assert r.output == str(samplefile) + '\n'
 
 if __name__ == '__main__':
     main(__file__)
