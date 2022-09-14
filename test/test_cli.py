0546bc2ac0d79ef9252e372e8ee2e00b25cc8d63
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2022-09-14 20:52:05 +0200

    test_cli: set envvar instead of passing dict
    (because TESSDATA_PREFIX must reach libtesseract)

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,7 +1,6 @@
 from click.testing import CliRunner
 
 from test.base import main
-from os import environ
 from pathlib import Path
 from ocrd_utils import pushd_popd
 from ocrd_tesserocr.cli import ocrd_tesserocr_recognize
@@ -9,19 +8,21 @@ from ocrd_utils import disableLogging
 
 runner = CliRunner()
 
-def test_show_resource(tmpdir):
+def test_show_resource(tmpdir, monkeypatch):
     samplefile = Path(tmpdir, 'bar.traineddata')
     samplefile.write_text('bar')
-    r = runner.invoke(ocrd_tesserocr_recognize, ['-C', 'bar'], env={**environ, 'TESSDATA_PREFIX': str(tmpdir)})
+    monkeypatch.setenv('TESSDATA_PREFIX': str(tmpdir))
+    r = runner.invoke(ocrd_tesserocr_recognize, ['-C', 'bar'])
     assert not r.exit_code
     # XXX doesn't work <del>because shutil.copyfileobj to stdout won't be captured
     # by self.invoke_cli</del> Not sure why it does not work :(
     # assert r.output == 'bar'
 
-def test_list_all_resources(tmpdir):
+def test_list_all_resources(tmpdir, monkeypatch):
     samplefile = Path(tmpdir, 'foo.traineddata')
     samplefile.write_text('foo')
-    r = runner.invoke(ocrd_tesserocr_recognize, ['-L'], env={**environ, 'TESSDATA_PREFIX': str(tmpdir)})
+    monkeypatch.setenv('TESSDATA_PREFIX': str(tmpdir))
+    r = runner.invoke(ocrd_tesserocr_recognize, ['-L'])
     assert not r.exit_code
     # XXX same problem
     # assert r.output == str(samplefile) + '\n'
