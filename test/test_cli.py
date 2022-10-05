665ecd31a0f4f1193da00e6159df4cada6172db9
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2022-10-06 00:49:22 +0200

    test_cli: without database, -C test still needs suffix

--- test/test_cli.py
+++ test/test_cli.py
@@ -12,10 +12,12 @@ def test_show_resource(tmpdir, monkeypatch):
     samplefile.write_text('bar')
     # simulate a Tesseract compiled with custom tessdata dir
     monkeypatch.setenv('TESSDATA_PREFIX', str(tmpdir))
+    # does not work (thus, tesserocr must not have been loaded already):
+    #monkeypatch.delitem(sys.modules, 'tesserocr')
     # envvars influence tesserocr's module initialization
     from ocrd_tesserocr.cli import ocrd_tesserocr_recognize
-    r = runner.invoke(ocrd_tesserocr_recognize, ['-C', 'bar'])
-    assert not r.exit_code
+    r = runner.invoke(ocrd_tesserocr_recognize, ['-C', 'bar.traineddata'])
+    assert not r.exit_code, r.output
     # XXX doesn't work <del>because shutil.copyfileobj to stdout won't be captured
     # by self.invoke_cli</del> Not sure why it does not work :(
     # assert r.output == 'bar'
