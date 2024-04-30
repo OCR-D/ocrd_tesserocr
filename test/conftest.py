289d41e5fc189b19864394ee2424930d9aeca59f
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2024-05-01 00:08:13 +0200

    conftest: use tmpdir fixture, add sample

--- test/conftest.py
+++ test/conftest.py
@@ -6,16 +6,23 @@ from test.assets import assets as assets
 
 METS_KANT_BINARIZED = assets.url_of('kant_aufklaerung_1784-binarized/data/mets.xml')
 METS_HEROLD_SMALL = assets.url_of('SBB0000F29300010000/data/mets_one_file.xml')
+METS_GUTACHTEN = assets.url_of('gutachten/data/mets.xml')
 
 @fixture
-def workspace_kant_binarized():
+def workspace_kant_binarized(tmpdir):
     initLogging()
-    with pushd_popd(tempdir=True) as tempdir:
-        yield Resolver().workspace_from_url(METS_KANT_BINARIZED, dst_dir=tempdir, download=True)
+    with pushd_popd(tmpdir):
+        yield Resolver().workspace_from_url(METS_KANT_BINARIZED, dst_dir=tmpdir, download=True)
 
 @fixture
-def workspace_herold_small():
+def workspace_herold_small(tmpdir):
     initLogging()
-    with pushd_popd(tempdir=True) as tempdir:
-        yield Resolver().workspace_from_url(METS_HEROLD_SMALL, dst_dir=tempdir, download=True)
+    with pushd_popd(tmpdir):
+        yield Resolver().workspace_from_url(METS_HEROLD_SMALL, dst_dir=tmpdir, download=True)
+
+@fixture
+def workspace_gutachten(tmpdir):
+    initLogging()
+    with pushd_popd(tmpdir):
+        yield Resolver().workspace_from_url(METS_GUTACHTEN, dst_dir=tmpdir, download=True)
 
