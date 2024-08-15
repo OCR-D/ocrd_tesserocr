f6c5ea0f04a5953faa04221b1ae4311dd3f2570b
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2024-08-16 01:29:54 +0200

    binarize: use final v3 API

--- ocrd_tesserocr/binarize.py
+++ ocrd_tesserocr/binarize.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+from typing import Optional
 import os.path
 from tesserocr import (
     PyTessBaseAPI,
@@ -9,7 +10,9 @@ from tesserocr import (
 from ocrd_models.ocrd_page import (
     AlternativeImageType,
     TextRegionType,
+    OcrdPage
 )
+from ocrd.processor import OcrdPageResult, OcrdPageResultImage
 
 from .recognize import TesserocrRecognize
 
@@ -22,7 +25,7 @@ class TesserocrBinarize(TesserocrRecognize):
         # use default model (eng) with vanilla tesserocr API
         self.tessapi = PyTessBaseAPI()
 
-    def process_page_pcgts(self, pcgts, output_file_id=None, page_id=None):
+    def process_page_pcgts(self, *input_pcgts: Optional[OcrdPage], page_id : Optional[str] = None) -> OcrdPageResult:
         """Performs binarization of the region / line with Tesseract on the workspace.
         
         Open and deserialize PAGE input file and its respective images,
@@ -42,6 +45,8 @@ class TesserocrBinarize(TesserocrRecognize):
         sepmask = self.parameter['tiseg']
         oplevel = self.parameter['operation_level']
 
+        pcgts = input_pcgts[0]
+        result = OcrdPageResult(pcgts)
         page = pcgts.get_Page()
         page_image, page_xywh, page_image_info = self.workspace.image_from_page(
             page, page_id)
@@ -60,14 +65,11 @@ class TesserocrBinarize(TesserocrRecognize):
         self.logger.info("Binarizing on '%s' level in page '%s'", oplevel, page_id)
 
         if oplevel == 'page':
-            image = self._process_segment(-1, page, page_image, page_xywh,
-                                          page_id, output_file_id)
+            image = self._process_segment(-1, page, page_image, page_xywh, page_id)
             if image:
-                return [pcgts, image]
-            else:
-                return pcgts
+                result.images.append(image)
+            return result
 
-        result = [pcgts]
         regions = page.get_AllRegions(classes=['Text', 'Table'])
         if not regions:
             self.logger.warning("Page '%s' contains no text regions", page_id)
@@ -76,10 +78,9 @@ class TesserocrBinarize(TesserocrRecognize):
                 region, page_image, page_xywh)
             if oplevel == 'region':
                 image = self._process_segment(RIL.BLOCK, region, region_image, region_xywh,
-                                              "region '%s'" % region.id,
-                                              output_file_id + '_' + region.id)
+                                              "region '%s'" % region.id)
                 if image:
-                    result.append(image)
+                    result.images.append(image)
             elif isinstance(region, TextRegionType):
                 lines = region.get_TextLine()
                 if not lines:
@@ -89,14 +90,13 @@ class TesserocrBinarize(TesserocrRecognize):
                     line_image, line_xywh = self.workspace.image_from_segment(
                         line, region_image, region_xywh)
                     image = self._process_segment(RIL.TEXTLINE, line, line_image, line_xywh,
-                                                  "line '%s'" % line.id,
-                                                  output_file_id + '_' + region.id + '_' + line.id)
+                                                  "line '%s'" % line.id)
                     if image:
-                        result.append(image)
+                        result.images.append(image)
 
         return result
 
-    def _process_segment(self, ril, segment, image, xywh, where, file_id):
+    def _process_segment(self, ril, segment, image, xywh, where) -> Optional[OcrdPageResultImage]:
         self.tessapi.SetImage(image)
         features = xywh['features'] + ",binarized"
         image_bin = None
@@ -119,11 +119,8 @@ class TesserocrBinarize(TesserocrRecognize):
                 image_bin = layout.GetBinaryImage(ril)
         if not image_bin:
             self.logger.error('Cannot binarize %s', where)
-            return False
-        # update METS (add the image file):
-        file_id += '.IMG-BIN'
-        file_path = os.path.join(self.output_file_grp, file_id + '.png')
+            return None
         # update PAGE (reference the image file):
-        segment.add_AlternativeImage(AlternativeImageType(
-            filename=file_path, comments=features))
-        return image_bin, file_id, file_path
+        image_ref = AlternativeImageType(comments=features)
+        segment.add_AlternativeImage(image_ref)
+        return OcrdPageResultImage(image_bin, segment.id + '.IMG-BIN', image_ref)
